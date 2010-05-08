beea0905e946bd585479ffa068c95e429fb42feb
Sam Beran <sberan@gmail.com>
2010-05-08 14:20:51 -0500

    wire up behavior for hellopolly

--- 2B/HelloPoly/Controller.h
+++ 2B/HelloPoly/Controller.h
@@ -1,10 +1,17 @@
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
+#import "PolygonShape.h"
 
 @interface Controller : NSObject {
     IBOutlet UIButton *decreaseButton;
     IBOutlet UIButton *increaseButton;
     IBOutlet UILabel *numberOfSidesLabel;
+    IBOutlet UILabel *nameLabel;
+    IBOutlet UILabel *angleDegreesLabel;
+    IBOutlet UILabel *angleRadiansLabel;
+    IBOutlet UILabel *minimumNumberOfSidesLabel;
+    IBOutlet UILabel *maximumNumberOfSidesLabel;
+    IBOutlet PolygonShape *polygon;
 }
 - (IBAction)decrease:(id)sender;
 - (IBAction)increase:(id)sender;
--- 2B/HelloPoly/Controller.m
+++ 2B/HelloPoly/Controller.m
@@ -1,11 +1,30 @@
 #import "Controller.h"
 
 @implementation Controller
+
+- (void) updatePolygonBy:(int)delta {
+    [polygon setNumberOfSides:[polygon numberOfSides] + delta];
+    [nameLabel setText: [polygon name]];
+    [numberOfSidesLabel setText: [NSString stringWithFormat:@"%i", [polygon numberOfSides]]];
+    [minimumNumberOfSidesLabel setText: [NSString stringWithFormat:@"%i", [polygon minimumNumberOfSides]]];
+    [maximumNumberOfSidesLabel setText: [NSString stringWithFormat:@"%i", [polygon maximumNumberOfSides]]];
+    [angleDegreesLabel setText: [NSString stringWithFormat:@"%f", [polygon angleInDegrees]]];
+    [angleRadiansLabel setText: [NSString stringWithFormat:@"%f", [polygon angleInRadians]]];
+    [decreaseButton setEnabled: [polygon numberOfSides] > [polygon minimumNumberOfSides]];
+    [increaseButton setEnabled: [polygon numberOfSides] < [polygon maximumNumberOfSides]];
+     
+}
+
+- (void) awakeFromNib {
+    [polygon setNumberOfSides:5];
+    [self updatePolygonBy:0];
+}
+
 - (IBAction)decrease:(id)sender {
-    
+    [self updatePolygonBy: -1];
 }
 
 - (IBAction)increase:(id)sender {
-    
+    [self updatePolygonBy: 1];
 }
 @end
